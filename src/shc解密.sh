a62a4980b4e2f29adf787d9c2b5aa7ea21cac2c1
reigadegr <103645642+reigadegr@users.noreply.github.com>
2023-10-26 15:40:39 +0800

    Update shc解密.sh

--- src/shc解密.sh
+++ src/shc解密.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-shcfile=service.sh.x
-nohup $(pwd)/$shcfile >/dev/null 2>&1 &
+#!/system/bin/sh
+shcfile=$(pwd)/service.sh.x
+nohup $shcfile >$(pwd)/log.log 2>&1 &
 m=$(ps -ef | grep "$shcfile" | grep -v grep| awk '{print $2}')
 m2=$(ps -ef | grep "$shcfile" | grep -v grep| awk '{print $3}')
 cat /proc/$m/cmdline > shc解密.txt
